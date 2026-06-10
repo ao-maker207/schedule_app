079bd85b8980d96de73a8a89b38f618a42fe5602
unknown <aoi>
2026-06-10 16:02:54 +0900

    最終調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
   resources :schedules
-  get "posts/index"
-  get "users/index"
-  get "lessons/hello", to: "lessons#hello"
-  get "lessons/call", to: "lessons#call"
- # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-    resources :users
-    resources :posts
-  
+  root "schedules#index"
 end
